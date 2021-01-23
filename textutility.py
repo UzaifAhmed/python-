48d5f563629d8c36afb5d4f56dabfad08d919ee6
UzaifAhmed <51610781+UzaifAhmed@users.noreply.github.com>
2021-01-23 21:14:24 +0500

    How to make a Library

---
+++ textutility.py
@@ -0,0 +1,8 @@
+def charCount(paragraph):
+    """this func accepts a texting and returns an integr count of num of char in it"""
+    char_count = len(paragraph)
+    return char_count
+
+def word_count(paragraph):
+    word_count = len(paragraph.split(" "))
+    return word_count
